e6ce15dea3f7cfc62c4dbeb6c822d61904b45e64
Winmin3658 <113975386+Winmin3658@users.noreply.github.com>
2022-11-24 11:04:16 +0900

    user

---
+++ User.java
@@ -0,0 +1,34 @@
+package user;
+
+public class User {
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private String userArea;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserArea() {
+		return userArea;
+	}
+	public void setUserArea(String userArea) {
+		this.userArea = userArea;
+	}
+
+}
